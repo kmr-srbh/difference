0430abb79e427c01360156971d537df22ff5c20e
Saurabh Kumar <developer.saurabh@outlook.com>
2024-02-04 22:54:00 +0530

    Wrote tests for checking the lcs.

---
+++ tests/unittests/test_difference.py
@@ -0,0 +1,21 @@
+import unittest
+
+from sys import path
+
+path.append("../difference")
+from difference import Difference
+
+
+class TestDifference(unittest.TestCase):
+    def test_get_longest_common_subsequence(self):
+        self.assertEqual(Difference("", "").get_lcs(), "")
+        self.assertEqual(Difference("ABC", "XYZ").get_lcs(), "")
+        self.assertEqual(Difference("ABCD", "AC").get_lcs(), "AC")
+        self.assertEqual(Difference("AGCAT", "GAC").get_lcs(), "AC")
+        self.assertEqual(Difference("AABCXY", "XYZ").get_lcs(), "XY")
+        self.assertEqual(Difference("ABCDEF", "ABCDEF").get_lcs(), "ABCDEF")
+        self.assertEqual(Difference("XMJYAUZ", "MZJAWXU").get_lcs(), "MJAU")
+
+
+if __name__ == "__main__":
+    unittest.main()
